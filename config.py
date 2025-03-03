3f81ef96f76a55a104233cbaea05d91b7309dfcc
Vera Fesianava <verafes@gmail.com>
2025-03-03 12:13:15 -0800

    Add dotenv for env variables

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
-API_KEY = "529f4176d252c087860f9ac02cb0e267"
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+PEXELS_API_KEY = os.getenv('PEXELS_API_KEY')
 BASE_URL="http://api.openweathermap.org"
 GEO_END_POINT = "/geo/1.0/zip"
 WEATHER_END_POINT = "/data/2.5/weather"
